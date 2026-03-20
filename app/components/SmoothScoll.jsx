6ae1510fb5c1272f6e072ea38c0d42103016bec9
Uto <tmkatemuujin82@gmail.com>
2026-03-20 14:08:45 +0800

    Loading

--- app/components/SmoothScoll.jsx
+++ app/components/SmoothScoll.jsx
@@ -1,28 +1,63 @@
+// "use client";
+
+// import { useEffect } from "react";
+// import Lenis from "lenis";
+
+// export default function SmoothScroll() {
+//   useEffect(() => {
+//     const lenis = new Lenis({
+//       duration: 1.2,
+//       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+//       smoothWheel: true,
+//       wheelMultiplier: 1,
+//       touchMultiplier: 2,
+
+//     });
+
+//     function raf(time) {
+//       lenis.raf(time);
+//       requestAnimationFrame(raf);
+//     }
+
+//     requestAnimationFrame(raf);
+
+//     return () => lenis.destroy();
+//   }, []);
+
+//   return null;
+// }
 "use client";
 
 import { useEffect } from "react";
+import { usePathname } from "next/navigation";
 import Lenis from "lenis";
 
+let lenisInstance = null;
+
 export default function SmoothScroll() {
+  const pathname = usePathname();
+
   useEffect(() => {
-    const lenis = new Lenis({
+    lenisInstance = new Lenis({
       duration: 1.2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       smoothWheel: true,
-      wheelMultiplier: 1,
-      touchMultiplier: 2,
-      
     });
 
     function raf(time) {
-      lenis.raf(time);
+      lenisInstance.raf(time);
       requestAnimationFrame(raf);
     }
 
     requestAnimationFrame(raf);
-
-    return () => lenis.destroy();
+    return () => lenisInstance.destroy();
   }, []);
+
+  useEffect(() => {
+    if (lenisInstance) {
+      lenisInstance.scrollTo(0, { immediate: true });
+    }
+  }, [pathname]);
 
   return null;
 }
